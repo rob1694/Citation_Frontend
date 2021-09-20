7bf6a92120fcf2bf06e8f7361ad606d472b9fde2
rob1694 <robertb1694@gmail.com>
2021-09-20 10:24:17 -0400

    styles

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,10 +3,10 @@ import React from 'react'
 function MainPage() {
 
     return (
-        <div>
-            <h3>Please click on an above option to view current active Citations by need.</h3>
-            <h4>Enforcer: To issuse citations as an Enforcer or view given citations</h4>
-            <h4>Offender: To view or payoff active citations issued to an individual.</h4>
+        <div class = "container p-3 mb-2 bg-info text-white">
+            <h3 class = "text-dark">Please click on an above option to view current active Citations by need.</h3>
+            <h4 class = "text-dark">Enforcer: To issuse citations as an Enforcer or view given citations</h4>
+            <h4 class = "text-dark">Offender: To view or payoff active citations issued to an individual.</h4>
         </div>
     )
 }
